bc154053ece8cf493e943c702942b1d6b6dfa48c
harshdev2909 <harsh.dev2909@gmail.com>
2024-08-31 22:18:16 +0530

    UI Done

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
-import logo from './logo.svg';
 import './App.css';
+import Navbar from './components/Navbar';
+import Shop from './components/Shop';
 
 function App() {
   return (
-    <div className="App">
-      
+    <div>
+      <Navbar/>
+      <Shop/>
     </div>
   );
 }
